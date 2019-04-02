fe83e68a503fbb9fa63b2d1dbfde544117bb1249
Balint Maschio <bmaschio77@gmail.com>
2019-04-02 16:04:23 +0200

    First Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict'
+
+var bloomrun = require('bloomrun')();
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Esempio server HTTP\n');
+});
+
+
+const callback = () => {
+  const address = server.address().address;
+  const port = server.address().port;
+  console.log(`
+  Server avviato all'indirizzo http://${address}:${port}
+  `);
+}
+
+server
+  .listen(
+    8000,
+    '127.0.0.1',
+    callback
+  )
